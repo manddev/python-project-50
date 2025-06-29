abbe5e457f066ca360a0d024abb097be26be1ffd
manddev <malanddev@yandex.ru>
2025-06-29 09:35:05 +0300

    init commit

---
+++ gendiff/converter.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import json
+
+def get_full_path(file_name):
+    return Path(__file__).parent / 'tests' / 'fixtures' / file_name
+
+def get_data(file_name):
+    if file_name.endswith('.json'):
+        return json.load(open(get_full_path(file_name)))
+    
