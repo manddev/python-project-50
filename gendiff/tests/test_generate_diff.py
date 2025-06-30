a7d9945e832ee777a4885b16d5f1371a9d6861a2
manddev <malanddev@yandex.ru>
2025-06-30 09:45:59 +0300

    add tests

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,1 +1,11 @@
+from os import path
 from gendiff import generate_diff
+
+def test_gendiff():
+    current_dir = path.dirname(path.abspath(__file__))
+    actual = generate_diff('file1.json', 'file2.json')
+    expected = open(path.join(current_dir, 'test_data', 'expected_json.txt')).read()
+    actual == expected
+    
+    
+
