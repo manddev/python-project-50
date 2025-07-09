fa4f8fbd0e58f395d89f65b85524ed30ea3d9b37
manddev <malanddev@yandex.ru>
2025-07-09 09:54:40 +0300

    add yaml conversion

--- gendiff/converter.py
+++ gendiff/converter.py
@@ -1,3 +1,4 @@
+import yaml
 import json
 from pathlib import Path
 
@@ -10,3 +11,7 @@ def get_data(file_name):
     if file_name.endswith('.json'):
         return json.load(open(get_full_path(file_name)))
     
+    if file_name.endswith('.yaml') or file_name.endswith('.yml'):
+        fullpath = get_full_path(file_name)
+        with open(fullpath, 'r', encoding='utf-8') as file:
+            return yaml.safe_load(file)
