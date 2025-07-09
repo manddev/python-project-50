e3f7b94862985c2f714c85f1bd1a0685b34c8bb2
manddev <malanddev@yandex.ru>
2025-07-09 22:39:42 +0300

    add yaml test

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -2,13 +2,19 @@ from os import path
 
 from gendiff import generate_diff
 
+CURRENT_DIR = path.dirname(path.abspath(__file__))
 
-def test_gendiff():
-    current_dir = path.dirname(path.abspath(__file__))
+
+def test_json():
     actual = generate_diff('file1.json', 'file2.json')
-    expected = open(path.join(current_dir, 'test_data', 'expected_json.txt')) \
+    expected = open(path.join(CURRENT_DIR, 'test_data', 'expected_json.txt')) \
+        .read()
+    actual == expected
+
+
+def test_yaml():
+    actual = generate_diff('file1.yml', 'file2.yml')
+    expected = open(path.join(CURRENT_DIR, 'test_data', 'expected_json.txt')) \
         .read()
     actual == expected
     
-    
-
