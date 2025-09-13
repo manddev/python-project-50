f5ad4b86031532f765ea6e5b0c69fdcd13bdc58c
manddev <malanddev@yandex.ru>
2025-09-13 23:28:53 +0300

    fix code

--- gendiff/formatters/make_format.py
+++ gendiff/formatters/make_format.py
@@ -1,7 +1,7 @@
-from stylish import format_stylish
+from formatters.stylish import format_stylish
 
 
-def make_format(diff, fomtat="stylish"):
+def make_format(diff, format="stylish"):
     match(format):
         case "stylish":
             return format_stylish(diff)
