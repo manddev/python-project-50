9d22948cc4b20ef8c03f60eee89e39495e6ec445
manddev <malanddev@yandex.ru>
2025-09-26 09:37:51 +0300

    fix linter errors

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -27,28 +27,38 @@ def format_stylish(diff, depth=1):
     for node in diff:
         name = node["name"]
         node_status = node["node_status"]
-        value = node.get("value", " ")
-        old_value = node.get("old_value", " ")
-        new_value = node.get("new_value", " ")
-        children = node.get("children", " ")
+        value = node.get("value")
+        old_value = node.get("old_value")
+        new_value = node.get("new_value")
+        children = node.get("children")
         
         match (node_status):
             case "added":
-                lines.append(f"{key_ind}+ \
-                             {name}: {stringify(value, depth + 1)}")
+                lines.append(
+                    f"{key_ind}+ {name}: {stringify(value, depth + 1)}"
+                )
+                             
             case "deleted":
-                lines.append(f"{key_ind}- \
-                             {name}: {stringify(value, depth + 1)}")
+                lines.append(
+                    f"{key_ind}- {name}: {stringify(value, depth + 1)}"
+                )
             case "unchanged":
-                lines.append(f"{key_ind}  \
-                             {name}: {stringify(value, depth + 1)}")
+                lines.append(
+                    f"{key_ind}  {name}: {stringify(value, depth + 1)}"
+                )
+                             
             case "modified":
-                lines.append(f"{key_ind}- \
-                             {name}: {stringify(old_value, depth + 1)}")
-                lines.append(f"{key_ind}+ \
-                             {name}: {stringify(new_value, depth + 1)}")
+                lines.append(
+                    f"{key_ind}- {name}: {stringify(old_value, depth + 1)}"
+                )
+                             
+                lines.append(
+                    f"{key_ind}+ {name}: {stringify(new_value, depth + 1)}"
+                )
+                             
             case "nested":
-                lines.append(f"{key_ind}  \
-                             {name}: {format_stylish(children, depth + 1)}")
-
+                lines.append(
+                    f"{key_ind}  {name}: {format_stylish(children, depth + 1)}"
+                )
+                             
     return "{\n" + "\n".join(lines) + f"\n{close_ind}}}"
