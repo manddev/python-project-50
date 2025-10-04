236c6524d19388f781e01b2112fe5718decc56cd
manddev <malanddev@yandex.ru>
2025-10-04 09:34:03 +0300

    fix generate_diff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -7,4 +7,8 @@ def generate_diff(filepath1, filepath2, format='stylish'):
     dict1 = get_data(filepath1)
     dict2 = get_data(filepath2)
     diff = get_diff(dict1, dict2)
-    return make_format(diff)
+    return make_format(diff, format)
+
+
+
+
