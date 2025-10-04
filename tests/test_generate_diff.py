d52e864458b605f8ff3fb2949d7f19407f19ef21
manddev <malanddev@yandex.ru>
2025-10-04 09:34:46 +0300

    fix linter warnings

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -7,27 +7,35 @@ CURRENT_DIR = path.dirname(path.abspath(__file__))
 
 def test_json():
     actual = generate_diff('file1.json', 'file2.json')
-    expected = open(path.join(CURRENT_DIR, 'test_data', 'expected_stylish.txt')) \
+    expected = open(
+        path.join(CURRENT_DIR, 'test_data', 'expected_stylish.txt')
+        ) \
         .read()
     actual == expected
 
 
 def test_yaml():
     actual = generate_diff('file1.yml', 'file2.yml')
-    expected = open(path.join(CURRENT_DIR, 'test_data', 'expected_stylish.txt')) \
+    expected = open(
+        path.join(CURRENT_DIR, 'test_data', 'expected_stylish.txt')
+        ) \
         .read()
     actual == expected
 
 
 def test_json_plain():
     actual = generate_diff('file1.json', 'file2.json', 'plain')
-    expected = open(path.join(CURRENT_DIR, 'test_data', 'expected_plain.txt')) \
+    expected = open(
+        path.join(CURRENT_DIR, 'test_data', 'expected_plain.txt')
+        ) \
         .read()
     actual == expected
 
 
 def test_yaml_plain():
     actual = generate_diff('file1.json', 'file2.json', 'plain')
-    expected = open(path.join(CURRENT_DIR, 'test_data', "expected_plain.txt")) \
+    expected = open(
+        path.join(CURRENT_DIR, 'test_data', "expected_plain.txt")
+        ) \
         .read()
     actual == expected    
